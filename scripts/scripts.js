d45c780990aba6b40568f9f0d81cd8a3f78576b9
David Gomez <david.gomez04@hotmail.com>
2022-11-20 19:52:49 -0500

    nuevos cambios en el formato y nuevos iconos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,29 @@
+let botonVerMas = document.getElementById("botonVerMas");
+let verMas = document.getElementById("verMas");
 
-let botonVerMas = document.getElementById('botonVerMas');
-let verMas = document.getElementById('verMas');
-
-botonVerMas.addEventListener('click', toggleText);
+botonVerMas.addEventListener("click", toggleText);
 
 function toggleText() {
-    verMas.classList.toggle('mostrar');
+  verMas.classList.toggle("mostrar");
 
-    if(verMas.classList.contains('mostrar')) {
-        botonVerMas.innerHTML = "ver menos categorias";
-    } else {
-        botonVerMas.innerHTML = "Ver mas categorias";
-    }
+  if (verMas.classList.contains("mostrar")) {
+    botonVerMas.innerHTML = "ver menos categorias";
+  } else {
+    botonVerMas.innerHTML = "Ver mas categorias";
+  }
+}
 
-}   
+let botonVerMasUno = document.getElementById("botonVerMasUno");
+let verMasUno = document.getElementById("verMasUno");
 
+botonVerMasUno.addEventListener("click", toggleText);
+
+function toggleText() {
+  verMasUno.classList.toggle("mostrar");
+
+  if (verMasUno.classList.contains("mostrar")) {
+    botonVerMasUno.innerHTML = "ver menos";
+  } else {
+    botonVerMasUno.innerHTML = "Ver mas";
+  }
+}
